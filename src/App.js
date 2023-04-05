2b1515232f481f59fa9843f94296efa4fb9666ed
Lisixo <lisixodev@gmail.com>
2023-04-05 20:23:43 +0200

    Create requester for student.json

--- src/App.js
+++ src/App.js
@@ -1,9 +1,31 @@
+import { useEffect, useState } from 'react';
 import './App.css';
 import { Navigation } from './components/navigation';
 
 function App() {
+  const [dataState, setDataState] = useState('requesting')
+  const [data, setData] = useState({ students: [] })
+
+  useEffect(() => {
+    // Anonimowa asynchroniczna funkcja
+    (
+      async () => {
+        // Wyłapywanie błędów
+        try{
+          const res = await fetch('/students.json')
+          const data = await res.json()
+
+          setData(data)
+          setDataState('ok')
+        } catch(e) {
+          console.error(e)
+          setDataState('error')
+        }
+      }
+    )()
+  })
   return <>
-    <Navigation />
+    <Navigation dataStatus={dataState} />
   </>
 }
 
